f83a16c45252db060154eb2ccb59cc754875fcfe
TIC <TIC@PCFORMACION015.ayesa.es>
2017-11-08 20:31:59 +0100

    8/11

--- src/main/java/api/resources/SongResource.java
+++ src/main/java/api/resources/SongResource.java
@@ -23,62 +23,90 @@ import model.repository.MapPlaylistRepository;
 import model.repository.PlaylistRepository;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collection;
 import java.util.List;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Path("/songs")
 public class SongResource {
-
-	public static SongResource _instance=null;
-	PlaylistRepository repository;
-	
-	private SongResource(){
-		repository=MapPlaylistRepository.getInstance();
-	}
-	
-	public static SongResource getInstance()
-	{
-		if(_instance==null)
-			_instance=new SongResource();
-		return _instance; 
-	}
-	
-	@GET
-	@Produces("application/json")
-	public Collection<Song> getAll()
-	{
-		return null;
-	}
-	
-	
-	@GET
-	@Path("/{id}")
-	@Produces("application/json")
-	public Song get(@PathParam("id") String songId)
-	{
-		
-		return null;
-	}
-	
-	@POST
-	@Consumes("application/json")
-	@Produces("application/json")
-	public Response addSong(@Context UriInfo uriInfo, Song song) {
-		return null;
-	}
-	
-	
-	@PUT
-	@Consumes("application/json")
-	public Response updateSong(Song song) {
-		return null;
-	}
-	
-	@DELETE
-	@Path("/{id}")
-	public Response removeSong(@PathParam("id") String songId) {
-		return null;
-	}
-	
+    
+    public static SongResource _instance = null;
+    PlaylistRepository repository;
+    
+    private SongResource() {
+        repository = MapPlaylistRepository.getInstance();
+    }
+    
+    public static SongResource getInstance() {
+        if (_instance == null) {
+            _instance = new SongResource();
+        }
+        return _instance;
+    }
+    
+    @GET
+    @Produces("application/json")
+    public Collection<Song> getAll() {
+        return repository.getAllSongs();
+    }
+    
+    @GET
+    @Path("/{id}")
+    @Produces("application/json")
+    public Song get(@PathParam("id") String songId) {
+        Song resultado = repository.getSong(songId);
+        if (resultado == null) {
+            throw new NotFoundException("La cancion " + songId + " no existe en la base de datos");
+        }
+        
+        return resultado;
+    }
+    
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response addSong(@Context UriInfo uriInfo, Song song) {
+        if (song.getTitle() != null && !song.getTitle().equals("")) {
+            repository.addSong(song);
+        } else {
+            throw new BadRequestException("Las canciones deben tener título");
+        }
+        URI resultado = null;
+        try {
+            resultado = new URI("");
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(SongResource.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ResponseBuilder resp = Response.created(resultado);
+        resp.entity(song);
+        
+        return resp.build();
+    }
+    
+    @PUT
+    @Consumes("application/json")
+    public void updateSong(Song song) {
+        Song oldSong = repository.getSong(song.getId());
+        if (oldSong == null) {
+            throw new NotFoundException("La canción que desea modificar no existe");
+        }
+        if (song.getTitle() == null || song.getTitle().equals("")) {
+            throw new BadRequestException("Las canciones deben tener título");
+        }
+        repository.updateSong(song);
+    }
+    
+    @DELETE
+    @Path("/{id}")
+    public void removeSong(@PathParam("id") String songId) {
+        Song song = repository.getSong(songId);
+        if (song == null) {
+            throw new NotFoundException("La canción que desea modificar no existe");
+        }
+        repository.deleteSong(songId);
+        
+    }
+    
 }
